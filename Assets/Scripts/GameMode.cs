13191510f6a2ae7acee32f7574c18ac7b1f04d34
Ryan Ash <scarletspark@gmail.com>
2023-07-08 17:21:14 +0200

    main script stubs added

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        // reset world state
+        // hide ui
+        // open inn / global map (decide later)
+        Debug.Log("Start Game");
+    }
+}
