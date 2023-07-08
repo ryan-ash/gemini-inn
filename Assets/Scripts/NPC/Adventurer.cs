62b7b53c90a7497fc58025aa4e33a2da5a711699
Vladyslav <cropon@gmail.com>
2023-07-08 23:35:15 +0300

    Adventurer init

---
+++ Assets/Scripts/NPC/Adventurer.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Adventurer : MonoBehaviour
+{
+    [SerializeField]
+    private SkinnedMeshRenderer HeadRenderer;
+    [SerializeField]
+    private SkinnedMeshRenderer BodyRenderer;
+    [SerializeField]
+    private SkinnedMeshRenderer LegsRenderer;
+    [SerializeField]
+    private SkinnedMeshRenderer BackpackRenderer;
+    [SerializeField]
+    private SkinnedMeshRenderer FeetRenderer;
+    
+
+    [SerializeField]
+    private List<Color> SkinViableColors;
+    [SerializeField]
+    private List<Color> HairViableColors;
+    [SerializeField]
+    private List<Color> ViableColors;
+    [SerializeField]
+    private List<Mesh> BodyVariations;
+    [SerializeField]
+    private List<Mesh> HeadVariations;
+    [SerializeField]
+    private List<Mesh> LegsVariations;
+    [SerializeField]
+    private List<Mesh> BackpackVariations;
+    [SerializeField]
+    private List<Mesh> FeetVariations;
+
+    public void RandomizeCharacter()
+    {
+        int Index = Random.Range(0, HeadVariations.Count);
+        HeadRenderer.sharedMesh = HeadVariations[Index];
+        //HeadRenderer.materials = HeadVariations[Index].materials;
+        Index = Random.Range(0, BodyVariations.Count);
+        BodyRenderer.sharedMesh = BodyVariations[Index];
+        //BodyRenderer.materials = BodyVariations[Index].materials;
+        Index = Random.Range(0, LegsVariations.Count);
+        LegsRenderer.sharedMesh = LegsVariations[Index];
+        //LegsRenderer.materials = LegsVariations[Index].materials;
+        Index = Random.Range(0, FeetVariations.Count);
+        FeetRenderer.sharedMesh = FeetVariations[Index];
+        //FeetRenderer.materials = FeetRenderer[Index].materials;
+
+        Index = Random.Range(0, SkinViableColors.Count);
+        Color SkinColor = SkinViableColors[Index];
+        Index = Random.Range(0, HairViableColors.Count);
+        Color HairColor = HairViableColors[Index];
+        ChangeRendererColors(HeadRenderer, SkinColor, HairColor);
+        ChangeRendererColors(BodyRenderer, SkinColor, HairColor);
+        ChangeRendererColors(LegsRenderer, SkinColor, HairColor);
+        ChangeRendererColors(FeetRenderer, SkinColor, HairColor);
+        ChangeRendererColors(BackpackRenderer, SkinColor, HairColor);
+    }
+
+    private void ChangeRendererColors(SkinnedMeshRenderer Renderer, Color SkinColor, Color HairColor)
+    {
+        
+        Material[] Materials = Renderer.materials;
+
+        for (int I = 0; I < Materials.Length; I++)
+        {
+            if (Materials[I].name == "Skin")
+            {
+                Materials[I].SetColor("_Color", SkinColor);
+            }
+            else if (Materials[I].name == "Eyebrows" || Materials[I].name == "Hair")
+            {
+                Materials[I].SetColor("_Color", HairColor);
+            }
+            else
+            {
+                int Index = Random.Range(0, ViableColors.Count);
+                Materials[I].SetColor("_Color", ViableColors[Index]);
+            }
+        }
+    }
+
+    void Start()
+    {
+        RandomizeCharacter();
+    }
+}
